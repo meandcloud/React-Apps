b851ae92045d3b54534ca42442422d9f0734dd66
meandcloud <77702438+meandcloud@users.noreply.github.com>
2021-03-24 01:46:30 -0500

    updated project001
    updated project001

--- project001/src/App.js
+++ project001/src/App.js
@@ -6,6 +6,9 @@ function App() {
         <h1>This is my React App</h1>
         <h3>I will build application step by step</h3>
         <h4>My Name is Amar And I'm Web Developer</h4>
+        <p>
+          This is my new application
+        </p>
     </div>
   );
 }
